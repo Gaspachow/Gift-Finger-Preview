f25236a35908d6bdb7dcc5c2612e773224eab9d6
Gas <gaspardsmets@gmail.com>
2019-01-12 16:35:11 +0100

    added message at the end

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@ var videocontainer = document.getElementById('myVideo');
 var newmp4 = 'videos/2.mp4';
 var myButton = document.querySelector('button')
 var buttondisplay = 'block'
+var header = document.getElementById('header')
+
 videocontainer.addEventListener('ended',myHandler,false);
 function myHandler(e) {
     videocontainer.pause();
@@ -18,6 +20,10 @@ function myHandler(e) {
     videocontainer.removeEventListener('ended',myHandler,false);
 };
 
+function fuckYou() {
+    header.textContent = 'Fuck you, buddy.';
+    videocontainer.removeEventListener('ended',fuckYou,false);
+}
 
 //Button to open gift
 
@@ -25,7 +31,10 @@ myButton.onclick = function() {
     videocontainer.setAttribute('src','videos/3.mp4');
     newmp4 = 'videos/4.mp4'
     videocontainer.loop = false;
-    videocontainer.addEventListener('ended',myHandler,false);
+    videocontainer.addEventListener('ended',myHandler,false)
+    videocontainer.addEventListener('ended',fuckYou,false)
     buttondisplay = 'none';
+    myButton.style.display = buttondisplay;
 
 };
+
