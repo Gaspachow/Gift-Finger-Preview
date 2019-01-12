cb6f1e544f1ec78a5c472d6ed65bfe710de68dd0
Gas <gaspardsmets@gmail.com>
2019-01-12 15:24:20 +0100

    Fully functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,11 @@
 
-//Button to change name
-var myButton = document.querySelector('button')
-myButton.onclick = function() {
-    video2();
-};
 
 //video script
 
 
 var videocontainer = document.getElementById('myVideo');
 var newmp4 = 'videos/2.mp4';
+var myButton = document.querySelector('button')
 
 videocontainer.addEventListener('ended',myHandler,false);
 function myHandler(e) {
@@ -18,5 +14,17 @@ function myHandler(e) {
     videocontainer.load();
     videocontainer.play();
     videocontainer.loop = true;
+    myButton.style.display = "block";
     videocontainer.removeEventListener('ended',myHandler,false);
+};
+
+
+//Button to open gift
+
+myButton.onclick = function() {
+    videocontainer.setAttribute('src','videos/3.mp4');
+    newmp4 = 'videos/4.mp4'
+    videocontainer.loop = false;
+    videocontainer.addEventListener('ended',myHandler,false);
+
 }
