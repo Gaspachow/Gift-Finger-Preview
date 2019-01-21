3461f870094e0b61a89ce217e28857692c250864
Gas <gaspardsmets@gmail.com>
2019-01-21 23:29:57 +0100

    no more flash between videos

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,21 @@
 
 
 var videocontainer = document.getElementById('myVideo');
+var videocontainer2 = document.getElementById('myVideo2');
+var videocontainer3 = document.getElementById('myVideo3');
 var newmp4 = 'videos/2.mp4';
-var myButton = document.querySelector('button')
-var buttondisplay = 'block'
-var header = document.getElementById('header')
+var myButton = document.querySelector('button');
+var buttondisplay = 'block';
+var header = document.getElementById('header');
 
 videocontainer.addEventListener('ended',myHandler,false);
 function myHandler(e) {
     videocontainer.pause();
-    videocontainer.setAttribute('src', newmp4);
-    videocontainer.load();
-    videocontainer.play();
-    videocontainer.loop = true;
+    videocontainer2.setAttribute('class', "shown_video");
+    //videocontainer.setAttribute('class', "hidden_video");
+    videocontainer2.load();
+    videocontainer2.play();
+    videocontainer2.loop = true;
     myButton.style.display = buttondisplay;
     videocontainer.removeEventListener('ended',myHandler,false);
 };
@@ -28,9 +31,12 @@ function fuckYou() {
 //Button to open gift
 
 myButton.onclick = function() {
-    videocontainer.setAttribute('src','videos/3.mp4');
+    videocontainer2.setAttribute('class','hidden_video');
+    videocontainer3.setAttribute('class', 'shown_video')
+    videocontainer3.load();
+    videocontainer3.play();
     //newmp4 = 'videos/4.mp4'
-    videocontainer.loop = false;
+    videocontainer3.loop = false;
     //videocontainer.addEventListener('ended',myHandler,false)
     //videocontainer.addEventListener('ended',fuckYou,false)
     buttondisplay = 'none';
