d8b42e56e518054bcbde0cb85e23eea31fb9b93e
Gas <gaspardsmets@gmail.com>
2019-02-03 00:42:42 +0100

    Added Yubi idle pose

--- scripts/main.js
+++ scripts/main.js
@@ -6,41 +6,42 @@
 var videocontainer = document.getElementById('myVideo');
 var videocontainer2 = document.getElementById('myVideo2');
 var videocontainer3 = document.getElementById('myVideo3');
-var newmp4 = 'videos/2.mp4';
+var videocontainer4 = document.getElementById('myVideo4');
 var myButton = document.querySelector('button');
 var buttondisplay = 'block';
 var header = document.getElementById('header');
 
-videocontainer.addEventListener('ended',myHandler,false);
-function myHandler(e) {
+videocontainer.addEventListener('ended',replaceVid,false);
+
+function replaceVid(e) {
     videocontainer.pause();
     videocontainer2.setAttribute('class', "shown_video");
-    //videocontainer.setAttribute('class', "hidden_video");
     videocontainer2.load();
     videocontainer2.play();
     videocontainer2.loop = true;
     myButton.style.display = buttondisplay;
-    videocontainer.removeEventListener('ended',myHandler,false);
+    videocontainer.removeEventListener('ended',replaceVid,false);
 };
 
-function fuckYou() {
-    header.textContent = '';
-    videocontainer.removeEventListener('ended',fuckYou,false);
-}
+function replaceVidIdle(e) {
+    videocontainer.pause();
+    videocontainer4.setAttribute('class', "shown_video");
+    videocontainer4.load();
+    videocontainer4.play();
+    videocontainer4.loop = true;;
+};
+
 
 //Button to open gift
 
 myButton.onclick = function() {
-    //videocontainer2.setAttribute('class','hidden_video');
     videocontainer3.setAttribute('class', 'shown_video')
     videocontainer3.load();
     videocontainer3.play();
-    //newmp4 = 'videos/4.mp4'
     videocontainer3.loop = false;
-    //videocontainer.addEventListener('ended',myHandler,false)
-    //videocontainer.addEventListener('ended',fuckYou,false)
     buttondisplay = 'none';
     myButton.style.display = buttondisplay;
+    videocontainer3.addEventListener('ended',replaceVidIdle,false)
 
 };
 
