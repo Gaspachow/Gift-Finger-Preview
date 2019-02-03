d158946754a9cb7194894b8a6b30037d0e031ee4
Gas <gaspardsmets@gmail.com>
2019-02-03 01:09:39 +0100

    Added time function

--- scripts/main.js
+++ scripts/main.js
@@ -45,3 +45,29 @@ myButton.onclick = function() {
 
 };
 
+
+
+// Time display function
+
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    // add a zero in front of numbers<10
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+  startTime();
